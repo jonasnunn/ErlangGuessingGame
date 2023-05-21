68c5cc751b02c888005576b522c978ae111477e8
jonasnunn <jonasnunn@gmail.com>
2023-05-20 23:40:28 -0600

    mvp working

--- README.md
+++ README.md
@@ -1,26 +1,17 @@
 # Overview
-
-{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
-
-{Provide a description for the software that you wrote to demonstrate the Erlang language.}
-
-{Describe your purpose for writing this software.}
-
-{Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the software running and a walkthrough of the code.  Focus should be on sharing what you learned about the language syntax.}
+I created a guess the number game. I had never used a functional programming languages and so I wanted to make something that would require me to learn the syntax.
 
 [Software Demo Video](http://youtube.link.goes.here)
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
-
-{Describe the programming language that you used and any libraries.}
+I used VS Code with an erlang extension to develop this. I did not use any external libraries just vanilla erlang.
 
 # Useful Websites
 
 {Make a list of websites that you found helpful in this project}
-* [Web Site Name](http://url.link.goes.here)
-* [Web Site Name](http://url.link.goes.here)
+* [Web Site Name](https://www.youtube.com/watch?v=IEhwc2q1zG4&t=1092s)
+* [Web Site Name](https://www.oreilly.com/)
 
 # Future Work
 
--- main.erl
+++ main.erl
@@ -1,30 +1,41 @@
 -module(main).
-
 -import(string, [len/1, concat/2, to_lower/1]).
-
 -export([main/0]).
 
 main() ->
     io:fwrite("Welcome to the guessing game.\n"),
     RandNum = gen_rand_num(10),
-    UserGuess = get_guess(),
-    io:fwrite("Your guess is: ~p Answer: ~p\n", [UserGuess, RandNum]).
+    game_loop(RandNum).
 
+% The game loop that calls all the other functions
+game_loop(RandNum) ->
+    UserGuess = get_guess(),
+    io:fwrite("Your guess is: ~p Answer: ~p\n", [UserGuess, RandNum]),
+    compare(RandNum, UserGuess),
+    game_loop(RandNum).
+
+% Generates a random number from 1 to MaxNum
 gen_rand_num(MaxNum) ->
     rand:uniform(MaxNum).
 
-
+% Get the guess, make sure it only has valid characters and then convert it to an integer.
 get_guess() ->
-    io:fwrite("Enter a guess (1-10):"),
-    % GuessStr = string:strip(io:get_line(""), right, $\n),
+    io:fwrite("Enter a guess (1-10): "),
     GuessStr = io:get_line(""),
-    % Guess = string:strip(GuessStr, both, $\n),
-    try
-        Guess = list:to_integer(GuessStr),
-        Guess
-    catch
-        _:_ -> 
-            io:fwrite("Invalid Input.\n"),
-            get_guess()
+    Guess = string:strip(GuessStr, right, $\n),
+    GuessNum = list_to_integer(Guess),
+    GuessNum.
+    
+% Compare the guess to the answer and tell the user if they were high or low.
+compare(RandNum, Guess) ->
+    if 
+        RandNum > Guess ->
+            io:fwrite("Your guess is too low\n");
+        RandNum < Guess ->
+            io:fwrite("Your guess is too high\n");
+        RandNum == Guess ->
+            io:fwrite("You guessed it!!!\n"),
+            halt(); % Terminate the program.
+        true ->
+            io:fwrite("Something odd happened.\n")
     end.
-
